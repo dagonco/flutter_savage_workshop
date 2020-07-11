3a3266ded251e9057a331ed32311ab523859ac08
David González <dagonco.dev@gmail.com>
2020-07-11 20:16:27 +0200

    Set LandingScreen as home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:savage/screens/landing/landing_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Yeah, I'm just an empty application."),
-        ),
-      ),
+      home: LandingScreen(),
     );
   }
 }
